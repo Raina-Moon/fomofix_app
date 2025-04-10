af34bc22a78b0c7c02b593ef16363cbd54ae7dbe
Raina Moon <mds6425@gmail.com>
2025-04-10 17:05:44 +0900

    🐛 fix: Update navigation path type casting in Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -26,7 +26,7 @@ const Header = () => {
 
 const handleNavigation = () => {
     const path:string = isLoggedIn && user ? `/dashboard/${user.id}` : "/login"
-    router.push(path);
+    router.push(path as any);
   };
 
 
