0c60af9b43c26ceafb4bb0f3536c9860a4807f3d
Raina Moon <mds6425@gmail.com>
2025-04-10 20:37:48 +0900

    ⚡️ improve: Wrap LinearGradient in KeyboardAvoidingView for better keyboard handling

--- app/login.tsx
+++ app/login.tsx
@@ -1,22 +1,29 @@
 import LoginForm from "@/components/LoginForm";
 import { useThemeColor } from "@/hooks/useThemeColor";
 import { LinearGradient } from "expo-linear-gradient";
+import { KeyboardAvoidingView, Platform } from "react-native";
 
 const page = () => {
   const backgroundColorStart = useThemeColor({}, "primary-300");
   const backgroundColorEnd = useThemeColor({}, "primary-600");
 
   return (
-    <LinearGradient
-      colors={[backgroundColorStart, backgroundColorEnd]}
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 20}
     >
-      <LoginForm />
-    </LinearGradient>
+      <LinearGradient
+        colors={[backgroundColorStart, backgroundColorEnd]}
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <LoginForm />
+      </LinearGradient>
+    </KeyboardAvoidingView>
   );
 };
 
