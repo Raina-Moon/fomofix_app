17a9f82538a7323d6d7413e2bd375b05fba190fa
Raina Moon <mds6425@gmail.com>
2025-04-10 17:04:02 +0900

    🎨improve : Enhance theme color management with improved type safety and additional color shades

--- hooks/useThemeColor.ts
+++ hooks/useThemeColor.ts
@@ -1,21 +1,30 @@
-/**
- * Learn more about light and dark modes:
- * https://docs.expo.dev/guides/color-schemes/
- */
-
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
+type ColorName = keyof typeof Colors.light & keyof typeof Colors.dark;
+type PrimaryShade = keyof typeof Colors.light.primary;
+type GrayShade = keyof typeof Colors.light.gray;
+
 export function useThemeColor(
   props: { light?: string; dark?: string },
-  colorName: keyof typeof Colors.light & keyof typeof Colors.dark
-) {
+  colorName: ColorName | `primary-${PrimaryShade}` | `gray-${GrayShade}`
+):string {
   const theme = useColorScheme() ?? 'light';
   const colorFromProps = props[theme];
 
   if (colorFromProps) {
     return colorFromProps;
-  } else {
-    return Colors[theme][colorName];
   }
+
+  if (colorName.startsWith("primary-")) {
+    const shade = colorName.split("-")[1] as unknown as PrimaryShade;
+    return Colors[theme].primary[shade];
+  }
+  if (colorName.startsWith("gray-")) {
+    const shade = colorName.split("-")[1] as unknown as GrayShade;
+    return Colors[theme].gray[shade];
+  }
+
+  const color = Colors[theme][colorName as keyof typeof Colors.light];
+  return typeof color === 'string' ? color : '';
 }
