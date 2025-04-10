fc400c31dc2c3d720e9d882cdac536b3d860082d
Raina Moon <mds6425@gmail.com>
2025-04-10 15:51:15 +0900

    ✨ feat: Add context providers for Bookmarks, Comments, Followers, Goals, Likes, Notifications, and Posts with associated state management and API interactions

---
+++ contexts/BookmarksContext.tsx
@@ -0,0 +1,73 @@
+import { createContext, useContext, ReactNode } from "react";
+import { useAuth } from "./AuthContext";
+import { Post } from "@/types";
+import { fetchApi } from "@/constants/api/fetch";
+
+interface BookmarksState {
+  bookmarkPost: (userId: number, postId: number) => Promise<void>;
+  unbookmarkPost: (userId: number, postId: number) => Promise<void>;
+  fetchBookmarkedPosts: (userId: number) => Promise<Post[]>;
+  fetchBookmarkedPostDetail: (userId: number) => Promise<Post[]>;
+}
+
+const BookmarksContext = createContext<BookmarksState | undefined>(undefined);
+
+export const BookmarksProvider = ({ children }: { children: ReactNode }) => {
+  const { token } = useAuth();
+
+  const bookmarkPost = async (userId: number, postId: number) => {
+    await fetchApi("/bookmarks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ userId, postId }),
+    });
+  };
+
+  const unbookmarkPost = async (userId: number, postId: number) => {
+    await fetchApi(`/bookmarks/${userId}/${postId}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  };
+
+  const fetchBookmarkedPosts = async (userId: number) => {
+    return await fetchApi<Post[]>(`/bookmarks/${userId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  };
+
+  const fetchBookmarkedPostDetail = async (userId: number) => {
+    return await fetchApi<Post[]>(`/bookmarks/${userId}/detailed`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  };
+
+  const value: BookmarksState = {
+    bookmarkPost,
+    unbookmarkPost,
+    fetchBookmarkedPosts,
+    fetchBookmarkedPostDetail,
+  };
+
+  return (
+    <BookmarksContext.Provider value={value}>
+      {children}
+    </BookmarksContext.Provider>
+  );
+};
+
+export const useBookmarks = () => {
+  const context = useContext(BookmarksContext);
+  if (!context)
+    throw new Error("useBookmarks must be used within a BookmarksProvider");
+  return context;
+};
