7df7366d65be9fb23a6c2905536272d0e216f05e
Raina Moon <mds6425@gmail.com>
2025-04-11 23:56:14 +0900

    💄ui: Wrap ForgotPasswordForm in LinearGradient for enhanced styling

--- app/forgot-password.tsx
+++ app/forgot-password.tsx
@@ -1,12 +1,24 @@
 import { Keyboard, TouchableWithoutFeedback, View } from "react-native";
 import ForgotPasswordForm from "@/components/forgot_password/ForgotPasswordForm";
+import { Line } from "react-native-svg";
+import { LinearGradient } from "expo-linear-gradient";
+import { useThemeColor } from "@/hooks/useThemeColor";
 
 const page = () => {
+  const white = "#fff";
+  const primary300 = useThemeColor({}, "primary-300");
+  const primary600 = useThemeColor({}, "primary-600");
+
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={{ flex: 1 }}>
-        <ForgotPasswordForm />
-      </View>
+      <LinearGradient
+        style={{ flex: 1 }}
+        colors={[white, primary300, primary600]}
+      >
+        <View style={{ flex: 1 }}>
+          <ForgotPasswordForm />
+        </View>
+      </LinearGradient>
     </TouchableWithoutFeedback>
   );
 };
