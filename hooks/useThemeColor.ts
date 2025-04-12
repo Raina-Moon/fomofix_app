f3c2f16f9d973f07081d3ed83e722b3e97c2d16b
Raina Moon <mds6425@gmail.com>
2025-04-12 21:34:52 +0900

    💄feat : Add red color shades to Colors constant and update useThemeColor hook

--- hooks/useThemeColor.ts
+++ hooks/useThemeColor.ts
@@ -4,10 +4,11 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 type ColorName = keyof typeof Colors.light & keyof typeof Colors.dark;
 type PrimaryShade = keyof typeof Colors.light.primary;
 type GrayShade = keyof typeof Colors.light.gray;
+type RedShade = keyof typeof Colors.light.red;
 
 export function useThemeColor(
   props: { light?: string; dark?: string },
-  colorName: ColorName | `primary-${PrimaryShade}` | `gray-${GrayShade}`
+  colorName: ColorName | `primary-${PrimaryShade}` | `gray-${GrayShade}` | `red-${RedShade}`
 ):string {
   const theme = useColorScheme() ?? 'light';
   const colorFromProps = props[theme];
@@ -23,6 +24,11 @@ export function useThemeColor(
   if (colorName.startsWith("gray-")) {
     const shade = colorName.split("-")[1] as unknown as GrayShade;
     return Colors[theme].gray[shade];
+  }
+
+if (colorName.startsWith("red-")) {
+    const shade = colorName.split("-")[1] as unknown as RedShade;
+    return Colors[theme].red[shade];
   }
 
   const color = Colors[theme][colorName as keyof typeof Colors.light];
