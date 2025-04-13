4be8ecc8475de91233e12167b3d7dc61023118b7
Raina Moon <mds6425@gmail.com>
2025-04-13 17:17:58 +0900

    🚑 fix: Integrate config into GluestackUIProvider for improved layout consistency

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,10 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import "@/global.css";
-import { GluestackUIProvider } from "@gluestack-ui/themed";
+import { GluestackUIProvider, StyledProvider } from "@gluestack-ui/themed";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
@@ -10,6 +14,13 @@ import { AuthProvider } from "@/contexts/AuthContext";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Toast from "react-native-toast-message";
 import Header from "@/components/Header";
+import { GoalProvider } from "@/contexts/GoalContext";
+import { PostProvider } from "@/contexts/PostContext";
+import { FollowerProvider } from "@/contexts/FollowerContext";
+import { config } from "@/components/ui/gluestack-ui-provider/config";
+import { LikesProvider } from "@/contexts/LikesContext";
+import { CommentsProvider } from "@/contexts/CommentsContext";
+import { BookmarksProvider } from "@/contexts/BookmarksContext";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -30,26 +41,59 @@ export default function RootLayout() {
   }
 
   return (
-    <GluestackUIProvider>
+    <GluestackUIProvider config={config}>
       <ThemeProvider value={DefaultTheme}>
         <AuthProvider>
-          <SafeAreaView style={{ flex: 1 }}>
-            <Toast />
-            <Stack
-              screenOptions={{
-                headerShown: true,
-                header: () => <Header />,
-              }}
-            >
-              <Stack.Screen name="login" options={{ headerShown: false }} />
-              <Stack.Screen name="signup" options={{ headerShown: false }} />
-              <Stack.Screen name="forgot-password" options={{ headerShown: false }} />
-              <Stack.Screen name="reset-password" options={{ headerShown: false }} />
-              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-              <Stack.Screen name="dashboard/[userId]" options={{ headerShown: true }} />
-              <Stack.Screen name="+not-found" options={{ headerShown: true }} />
-            </Stack>
-          </SafeAreaView>
+          <GoalProvider>
+            <PostProvider>
+              <FollowerProvider>
+                <BookmarksProvider>
+                  <LikesProvider>
+                    <CommentsProvider>
+                      <SafeAreaView style={{ flex: 1 }}>
+                        <Toast />
+                        <Stack
+                          screenOptions={{
+                            headerShown: true,
+                            header: () => <Header />,
+                          }}
+                        >
+                          <Stack.Screen
+                            name="login"
+                            options={{ headerShown: false }}
+                          />
+                          <Stack.Screen
+                            name="signup"
+                            options={{ headerShown: false }}
+                          />
+                          <Stack.Screen
+                            name="forgot-password"
+                            options={{ headerShown: false }}
+                          />
+                          <Stack.Screen
+                            name="reset-password"
+                            options={{ headerShown: false }}
+                          />
+                          <Stack.Screen
+                            name="(tabs)"
+                            options={{ headerShown: false }}
+                          />
+                          <Stack.Screen
+                            name="dashboard/[userId]"
+                            options={{ headerShown: true }}
+                          />
+                          <Stack.Screen
+                            name="+not-found"
+                            options={{ headerShown: true }}
+                          />
+                        </Stack>
+                      </SafeAreaView>
+                    </CommentsProvider>
+                  </LikesProvider>
+                </BookmarksProvider>
+              </FollowerProvider>
+            </PostProvider>
+          </GoalProvider>
         </AuthProvider>
       </ThemeProvider>
     </GluestackUIProvider>
