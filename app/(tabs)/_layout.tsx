cb753ce04a4e3bcf62b4a12787c379a3939bc3d7
Raina Moon <mds6425@gmail.com>
2025-04-19 14:47:58 +0900

    ♻️ refactor: Simplify TabLayout and RootLayout components by removing unused code and improving header options

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,47 +1,37 @@
 import { Tabs } from "expo-router";
 import { Platform } from "react-native";
-import { HapticTab } from "@/components/HapticTab";
-import { IconSymbol } from "@/components/ui/IconSymbol";
-import TabBarBackground from "@/components/ui/TabBarBackground";
-import { Colors } from "@/constants/Colors";
 import Header from "@/components/Header";
+import TabBarBackground from "@/components/ui/TabBarBackground";
+import { IconSymbol } from "@/components/ui/IconSymbol";
 
 export default function TabLayout() {
-  console.log("TabLayout rendering");
   return (
-      <Tabs
-        screenOptions={{
-          tabBarActiveTintColor: Colors.light.tint,
-          headerShown: true,
-          header: () => <Header />,
-          tabBarButton: HapticTab,
-          tabBarBackground: TabBarBackground,
-          tabBarStyle: Platform.select({
-            ios: {
-              position: "absolute",
-            },
-            default: {},
-          }),
+    <Tabs
+      screenOptions={{
+        header: () => <Header />,
+        tabBarActiveTintColor: "#1EBD7B",
+        tabBarBackground: TabBarBackground,
+        tabBarStyle: Platform.select({ ios: { position: "absolute" } }),
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol name="house.fill" size={24} color={color} />
+          ),
         }}
-      >
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: "Home",
-            tabBarIcon: ({ color }) => (
-              <IconSymbol size={28} name="house.fill" color={color} />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="explore"
-          options={{
-            title: "Explore",
-            tabBarIcon: ({ color }) => (
-              <IconSymbol size={28} name="paperplane.fill" color={color} />
-            ),
-          }}
-        />
-      </Tabs>
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: "Explore",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol name="paperplane.fill" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
