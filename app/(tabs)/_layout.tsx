dabb4ee22c3c3aa16e3b5fd2f8d8bf1612379057
Raina Moon <mds6425@gmail.com>
2025-04-20 19:50:52 +0900

    ⚰️ remove: Delete IconSymbol component and associated fallback files

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,7 +2,6 @@ import { Tabs } from "expo-router";
 import { Platform } from "react-native";
 import Header from "@/components/Header";
 import TabBarBackground from "@/components/ui/TabBarBackground";
-import { IconSymbol } from "@/components/ui/IconSymbol";
 
 export default function TabLayout() {
   return (
@@ -14,15 +13,6 @@ export default function TabLayout() {
         tabBarStyle: Platform.select({ ios: { position: "absolute" } }),
       }}
     >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol name="house.fill" size={24} color={color} />
-          ),
-        }}
-      />
     </Tabs>
   );
 }
